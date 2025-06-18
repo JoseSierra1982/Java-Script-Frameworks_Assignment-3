e0432a645d36bf3c0236893a7be6d0391342f092
Jose David Sierra R <ingenierojd2012@hotmail.com>
2025-06-18 14:48:28 -0400

    first commit

---
+++ app3/app3.js
@@ -0,0 +1,35 @@
+//minimal and flexible Node.js web framework used to build web servers and APIs
+import express from 'express'
+//required in ES modules (ESM) to explicitly tell Node.js that you're importing a JSON file
+import data from './data/topVisit.json' assert{type : 'json'}
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('welcome visiting Canada !');
+})
+
+//Get
+app.get('/visit', (req, res) => {
+  res.json(data);
+})
+
+//Create
+app.post('/', (req, res) => {
+  res.send("create demo using post()");
+})
+
+//Update
+app.put('/', (req, res) => {
+  res.send("update demo using put()");
+})
+
+//Delete
+app.delete('/',(req,res)=> {
+  res.send("delete demo using delete()")
+})
+
+app.listen(port, () => {
+  console.log(`Our app : http://localhost:${port}/`)
+})
